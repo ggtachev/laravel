f19597e1791c6c649d115c638354b6fd0b880cc5
Georgi Tachev <georgi.tachev.dev@gmail.com>
2025-05-15 21:51:41 +0300

    updated main title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Vue Hello World</title>
+    <title>GT Trading vue</title>
     @vite(['resources/js/app.js'])
 </head>
 <body>
